c36af20963457ce2f1e8e4271232871c2aa7f304
Enrique Redondo Cortés <eredondoco.inf@upsa.es>
2025-04-06 19:35:23 +0200

    refactor: reorganizar estructura de navegación
    Se eliminó el archivo de pruebas del componente AppComponent. Se incluyeron cambios en el diseño y estructura de navegación para mejorar la usabilidad, dividiendo claramente la interfaz en aside y contenido principal.

--- frontend/src/app/componentes/menu/menu.component.ts
+++ frontend/src/app/componentes/menu/menu.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {CambioMonedaComponent} from "../cambio-moneda/cambio-moneda.component";
 
 @Component({
   selector: 'app-menu',
     imports: [
-        RouterLink
+        RouterLink,
+        CambioMonedaComponent
     ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
