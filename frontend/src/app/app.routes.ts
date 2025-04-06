6bc8fdf3c7cca5e57ae07c1524c4b2de8c40f451
Enrique Redondo Cortés <eredondoco.inf@upsa.es>
2025-04-06 22:26:15 +0200

    feat: añadir ruta "activos" y refactorizar componente
    Se añadió una nueva ruta "activos" con su respectivo enlace en el menú. El componente de activos fue refactorizado para mostrar todos los activos o solo favoritos según la URL de navegación. Además, se actualizaron las rutas en "app.routes.ts" para reflejar estos cambios.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import {DashboardComponent} from "./paginas/dashboard/dashboard.component";
 import {ActivoComponent} from "./paginas/activo/activo.component";
 import {PreferenciasComponent} from "./paginas/preferencias/preferencias.component";
-import {Activos} from "./paginas/activos/activos";
+import {ActivosComponent} from "./paginas/activos/activos.component";
 
 export const routes: Routes = [
     {path: '', component: DashboardComponent},
-    {path: 'favoritos', component: Activos},
+    {path: 'activos', component: ActivosComponent},
+    {path: 'activos/favoritos', component: ActivosComponent},
     {path: 'preferencias', component: PreferenciasComponent},
     {path: 'activo/:simbolo', component: ActivoComponent},
 
---
+++ frontend/src/app/paginas/activos/activos.component.ts
@@ -0,0 +1,37 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {TarjetaActivoComponent} from "../../componentes/tarjeta-activo/tarjeta-activo.component";
+import {GestorApiService} from "../../servicios/gestor-api.service";
+import {DatosActivo} from "../../interfaces/datosActivo";
+import {Router, NavigationEnd} from "@angular/router";
+
+@Component({
+    selector: 'app-activos-favoritos',
+    imports: [
+        TarjetaActivoComponent
+    ],
+    templateUrl: './activos.component.html',
+    styleUrl: './activos.component.css'
+})
+export class ActivosComponent {
+    gestorApiService: GestorApiService = inject(GestorApiService);
+    router: Router = inject(Router);
+    datosSimbolos: DatosActivo[] | null = null;
+
+    constructor() {
+        this.actualizarDatosSimbolos();
+        this.router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                this.actualizarDatosSimbolos();
+            }
+        });
+    }
+
+    private actualizarDatosSimbolos() {
+        const url = this.router.url;
+        if (url === '/activos/favoritos') {
+            this.datosSimbolos = this.gestorApiService.datosSimbolosFavoritos();
+            return;
+        }
+        this.datosSimbolos = this.gestorApiService.obtenerDatosSimbolos();
+    }
+}
